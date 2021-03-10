2c5771b78f93075cd3d9eea46ac7104eb68acf4b
AashishChilwal <78070029+AashishChilwal@users.noreply.github.com>
2021-03-10 13:40:54 -0700

    Finished solving Q1

--- Q1.java
+++ Q1.java
@@ -1,1 +1,23 @@
-
+import java.util.Scanner;
+public class Q1
+{
+	public static void main(String[] args) {
+	Scanner letters = new Scanner(System.in);
+	System.out.println ("Enter a letter: " );
+    char letter = letters.next().charAt(0);
+    if (letter >= 97 && letter <=122)
+    {
+         if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
+       System.out.println(letter + " is a vowel.");
+      }
+      else{
+       System.out.println(letter + " is a consonant.");
+      }
+    }
+        else
+        {
+            System.out.println(letter+" is an invalid input");
+        }
+	    
+	}
+}
