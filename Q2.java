b3d1f2593af5b8aad86c95f59c5cad5a245aec10
AashishChilwal <78070029+AashishChilwal@users.noreply.github.com>
2021-03-10 15:48:38 -0700

    Update Q2.java

--- Q2.java
+++ Q2.java
@@ -1,1 +1,15 @@
-
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		 Scanner numberplate = new Scanner(System.in);
+		 System.out.print("Enter a valid plate number: ");
+		 String input = numberplate.nextLine();
+		 int integer = numberplate.nextInt();
+		 int integer1 = numberplate.nextInt();
+		 if (input.length() == 3)
+		 {
+		     System.out.println(" is a valid number plate.");
+		 }
+		 }
+		 }
