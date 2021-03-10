08535708f75f6bced0fa7d75a4ace97f63104d85
AashishChilwal <78070029+AashishChilwal@users.noreply.github.com>
2021-03-10 14:07:36 -0700

    Update Q3.java

--- Q3.java
+++ Q3.java
@@ -1,1 +1,27 @@
-
+import java.util.Scanner;
+public class Q3
+{
+	public static void main(String[] args) {
+	Scanner perimeter = new Scanner(System.in);
+	System.out.println ("Enter three edges (length in double): " );
+    double l1 = perimeter.nextDouble();
+    double l2 = perimeter.nextDouble();
+    double l3 = perimeter.nextDouble();
+    double sum = l1+l2 , sum1 = l2+l3 , sum2 = l1+l3;
+    double Perimeter = l1+l2+l3;
+    if (l1 >= 0 && l2>=0 && l3>=0)
+    {
+        if (sum > l3 || sum2>l2 || sum1>l1)
+        {
+            System.out.println("The perimeter of the triangle is: "+ Perimeter);
+        }
+        else{
+        System.out.println("Invalid input");
+    }
+    }
+    else
+    {
+        System.out.println("Invalid input");
+    }
+	}
+}
