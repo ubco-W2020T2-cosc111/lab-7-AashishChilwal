87aeebf55c075f67a21556aa157fcf79eaa97f11
AashishChilwal <78070029+AashishChilwal@users.noreply.github.com>
2021-03-10 16:19:44 -0700

    Update Q5.java

--- Q5.java
+++ Q5.java
@@ -1,1 +1,38 @@
+import util.java.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner checker = new Scanner(System.in)
+		System.out.println("Enter your salary: ");
+		int salary = checker.nextInt();
+		int tax = 0;
+		System.out.println("Enter M=married, S=single: ");
+		String status = checker.nextLine();
+		if (salary < 35,000 && )
+		{
+		    tax = (salary * (30/100));
+		    System.out.println("Tax$ : ");
+		} else if (salary < 35000 && )
+		{
+		    tax = (salary * (20/100));
+		    System.out.println("Tax$ : ");
+		}else if (salary > 50000 && )
+		{
+		    tax = (salary * (50/100));
+		    System.out.println("Tax$ : ");
+		}else if (salary > 50000 && )
+		{
+		    tax = (salary * (40/100));
+		    System.out.println("Tax$ : ");
+		}else if (salary >= 35000 && salary <=50000 && )
+		{
+		    tax = (salary * (40/100));
+		    System.out.println("Tax$ : ");
+		}else if (salary >= 35000 && salary <=50000 && )
+		{
+		    tax = (salary * (30/100));
+		    System.out.println("Tax$ : ");
+		}
+	}
+}
 
