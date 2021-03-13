3dec3a0eb357e3c3182b379adc0b2c14eceaf41a
AashishChilwal <78070029+AashishChilwal@users.noreply.github.com>
2021-03-12 18:57:54 -0700

    Finished Q5

--- Q5.java
+++ Q5.java
@@ -1,41 +1,46 @@
 
 import java.util.Scanner;
-public class Main
+public class Q5
 {
 	public static void main(String[] args) {
+	    int salary = 0, tax = 0;
 	    Scanner checker = new Scanner(System.in);
 		System.out.println("Enter your salary: ");
-		int salary = checker.nextInt();
-		int tax = 0;
+	    salary = checker.nextInt();
 		System.out.println("Enter M=married, S=single: ");
-		String [] Status = checker.next();
-		if (salary < 35000 && (Status[0].equals("M") || Status[0].equals("m")))
+		String status = checker.next();
+		for (int i = 0;i<1;i++)
 		{
-		    tax = (salary * (20/100));
+		    char ch = status.charAt(i);
+		
+		if (salary < 35000 && (ch == 'M'|| ch == 'm'))
+		{
+		     tax = salary * (20/100);
 		    System.out.println("Tax$ : " + tax);
-		} else if (salary < 35000 && status.charAt(0) == "S" || status.chartAt(0) == "s" )
+		} else if (salary < 35000 && (ch == 'S' || ch == 's') )
 		{
-		    tax = (salary * (30/100));
+		     tax = salary * (30/100);
 		    System.out.println("Tax$ : ");
-		}else if (salary > 50000 && status.charAt(0) == "M" || status.chartAt(0) == "m")
+		}else if (salary > 50000 && (ch == 'M' || ch == 'm'))
 		{
-		    tax = (salary * (40/100));
+		     tax = salary * (40/100);
 		    System.out.println("Tax$ : " + tax);
-		}else if (salary > 50000 && status.charAt(0) == "S" || status.chartAt(0) == "s")
+		}else if (salary > 50000 && (ch == 'S' || ch == 's'))
 		{
-		    tax = (salary * (50/100));
+		     tax = salary * (50/100);
 		    System.out.println("Tax$ : " + tax);
-		}else if (salary >= 35000 && salary <=50000 && status.charAt(0) == "S" || status.chartAt(0) == "s")
+		}else if ((salary >= 35000 && salary <=50000) && (ch == 'S' || ch == 's'))
 		{
-		    tax = (salary * (40/100));
+		     tax = salary * (40/100);
 		    System.out.println("Tax$ : " + tax);
-		}else if (salary >= 35000 && salary <=50000 && status.charAt(0) == "M" || status.chartAt(0) == "m")
+		}else if ((salary >= 35000 && salary <=50000) && (ch == 'M' || ch == 'm'))
 		{
-		    tax = (salary * (30/100));
+		     tax = salary * (30/100);
 		    System.out.println("Tax$ : " + tax);
 		}
 		else{
 		    System.out.println("Invalid Marital Status! ");
+		}
 		}
 	}
 }
